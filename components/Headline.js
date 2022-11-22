1e0f574ce3db0e1e446b68f4b5b89a7392d1823a
takuma terada <nsf3021@gmail.com>
2022-11-22 17:10:51 +0900

    css moduleのリファクタ

--- components/Headline.js
+++ components/Headline.js
@@ -1,10 +1,10 @@
-import styles from "../styles/Home.module.css";
+import classes from "./Headline.module.css";
 
 export function Headline(props) {
   return (
     <div>
-      <h1 className={styles.title}>aaaaa</h1>
-      <p className={styles.description}>
+      <h1 className={classes.title}>aaaaa</h1>
+      <p className={classes.description}>
         Get started by editing {props.children}
       </p>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import { Footer } from "../components/Footer";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { Footer } from "../components/Footer";
 import { Main } from "../components/Main";
 
 export default function Home() {
