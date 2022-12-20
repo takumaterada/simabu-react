220a2aa57a892e611cecaa1287394c8855ead934
takuma terada <nsf3021@gmail.com>
2022-12-20 18:16:53 +0900

    useeffect #10

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,20 +3,28 @@ import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
 import { Main } from "../components/Main";
 import { Header } from "../components/Header";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 export default function Home() {
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
+  // const handleClick = useCallback((e) => {
+  //   console.log(e.target.href);
+  //   e.preventDefault();
+  // }, []);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
   }, []);
 
   return (
     <div className={styles.container}>
       <Header />
-      <a href="/about" onClick={handleClick}>
+      {/* <a href="/about" onClick={handleClick}>
         ボタン
-      </a>
+      </a> */}
 
       <Main page="index" />
 
