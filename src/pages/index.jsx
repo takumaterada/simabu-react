9a091dfe79d398502ca20597bb8204a959bfb375
takuma terada <nsf3021@gmail.com>
2022-12-20 18:01:10 +0900

    #9

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,11 +3,20 @@ import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
 import { Main } from "../components/Main";
 import { Header } from "../components/Header";
+import { useCallback } from "react";
 
 export default function Home() {
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+  }, []);
+
   return (
     <div className={styles.container}>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
 
       <Main page="index" />
 
