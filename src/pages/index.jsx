2f473cd7257859a0c1a64ff1287e47f92da55147
takuma terada <nsf3021@gmail.com>
2022-12-21 11:13:52 +0900

    #11usestate

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,13 +3,14 @@ import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
 import { Main } from "../components/Main";
 import { Header } from "../components/Header";
-import { useCallback, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  // const handleClick = useCallback((e) => {
-  //   console.log(e.target.href);
-  //   e.preventDefault();
-  // }, []);
+  const [foo, setFoo] = useState(1);
+  // let foo = 1;
+  const handleClick = (e) => {
+    setFoo((foo) => foo + 1);
+  };
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -22,9 +23,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
-      {/* <a href="/about" onClick={handleClick}>
-        ボタン
-      </a> */}
+      <h1>{foo}</h1>
+      <button onClick={handleClick}>ボタン</button>
 
       <Main page="index" />
 
