20facfb4d3a29ee7aa618362cbebf825557399bd
takuma terada <nsf3021@gmail.com>
2022-12-26 01:40:57 +0900

    間違ったcommit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,16 @@ import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
 import { Main } from "../components/Main";
 import { Header } from "../components/Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState("true");
   // let foo = 1;
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-  };
+  const handleClick = useCallback((e) => {
+    setText(e.target.value);
+  }, []);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -23,8 +25,28 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
-      <h1>{foo}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
+      <button
+        onClick={() => {
+          setIsShow((isShow) => {
+            if (isShow === true) {
+              return false;
+            } else {
+              return true;
+            }
+          });
+        }}
+      >
+        非表示
+      </button>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      />
 
       <Main page="index" />
 
