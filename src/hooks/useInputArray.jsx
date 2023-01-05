e7add1afdf9726e641695beacbada1fe1f1609f9
takuma terada <nsf3021@gmail.com>
2023-01-05 11:54:59 +0900

    #15カスタムフック

---
+++ src/hooks/useInputArray.jsx
@@ -0,0 +1,24 @@
+import { useCallback, useState } from "react";
+
+export const useInputArray = () => {
+  const [text, setText] = useState("");
+  const [array, setArray] = useState([]);
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("５文字以内で");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      if (prevArray.some((item) => item === text)) {
+        alert("同じ要素がすでに存在します");
+        return prevArray;
+      }
+      return [...prevArray, text];
+    });
+  }, [text]);
+  return { text, array, handleChange, handleAdd };
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,65 +3,40 @@ import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
 import { Main } from "../components/Main";
 import { Header } from "../components/Header";
-import { useCallback, useEffect, useState } from "react";
+import { useCounter } from "src/hooks/useCounter";
+import { useInputArray } from "src/hooks/useInputArray";
+import { useBgLightBlue } from "src/hooks/useBgLightBlue";
 
 export default function Home() {
-  const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
-  const [isShow, setIsShow] = useState("true");
-  const [array, setArray] = useState([]);
-
-  const handleClick = useCallback((e) => {
-    setText(e.target.value);
-  }, []);
-
-  const handleDisplay = useCallback(() => {
-    setIsShow((prevIsShow) => !prevIsShow);
-  }, []);
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some((item) => item === text)) {
-        alert("同じ要素がすでに存在します");
-        return prevArray;
-      }
-      return [...prevArray, text];
-    });
-  }, [text]);
-
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-
-    return () => {
-      document.body.style.backgroundColor = "";
-    };
-  }, []);
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+  useBgLightBlue();
 
   return (
-    <div>
-      <div className={styles.container}>
-        <Header />
-        {isShow ? <h1>{count}</h1> : null}
-        <button onClick={handleClick}>ボタン</button>
-        <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
-        <input
-          type="text"
-          value={text}
-          onChange={(e) => {
-            setText(e.target.value);
-          }}
-        />
-        <button onClick={handleAdd}>追加</button>
-        <ul>
-          {array.map((item) => {
-            return <li key={item}>{item}</li>;
-          })}
-        </ul>
+    <div className={styles.container}>
+      <Header />
 
-        <Main page="index" />
+      {isShow ? <h1>{count}</h1> : null}
+      <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
 
-        <Footer />
-      </div>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
+
+      <Main page="index" />
+
+      <Footer />
     </div>
   );
 }
